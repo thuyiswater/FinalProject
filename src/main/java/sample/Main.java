6c654a89482d1d8e10f25ce8fc0a1c121dfac629
VoDaiDuong <67914364+VoDaiDuong@users.noreply.github.com>
2021-08-17 21:24:52 +0700

    added throws IOException

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,50 +6,50 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
 
-//        Parent root = FXMLLoader.load(getClass().getResource("/FXML/sample.fxml"));
-//        Scene scene = new Scene(root);
-//        primaryStage.setScene(scene);
-//        primaryStage.setTitle("Vietnam Fast News");
-//        primaryStage.show();
-//        primaryStage.setMaximized(true);
-//        primaryStage.setResizable(false);
-
-
-        Scraper scraper = new Scraper();
-        long start = System.currentTimeMillis();
-        scraper.getNew();
-        scraper.getCovid();
-        scraper.getPolitics();
-        scraper.getBusiness();
-        scraper.getTech();
-        scraper.getHealth();
-        scraper.getSports();
-        scraper.getEntertainment();
-        scraper.getWorld();
-        scraper.getOthers();
-        long end = System.currentTimeMillis();
-
-        System.out.println("Execution duration: " + (end - start));
-
-        System.out.println("The size of new: " + scraper.getNew().size());
-        System.out.println("The size of covid: " + scraper.getCovid().size());
-        System.out.println("The size of politic: " + scraper.getPolitics().size());
-        System.out.println("The size of business: " + scraper.getBusiness().size());
-        System.out.println("The size of technology: "  + scraper.getTech().size());
-        System.out.println("The size of health: " + scraper.getHealth().size());
-        System.out.println("The size of sport: " + scraper.getSports().size());
-        System.out.println("The size of entertainment: " + scraper.getEntertainment().size());
-        System.out.println("The size of world: " + scraper.getWorld().size());
-        System.out.println("The size of other: " + scraper.getOthers().size());
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/sample.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Vietnam Fast News");
+        primaryStage.show();
+        primaryStage.setMaximized(true);
+        primaryStage.setResizable(false);
 
 
+//        Scraper scraper = new Scraper();
+//        long start = System.currentTimeMillis();
+//        scraper.getNew();
+//        scraper.getCovid();
+//        scraper.getPolitics();
+//        scraper.getBusiness();
+//        scraper.getTech();
+//        scraper.getHealth();
+//        scraper.getSports();
+//        scraper.getEntertainment();
+//        scraper.getWorld();
+//        scraper.getOthers();
+//        long end = System.currentTimeMillis();
+//
+//        System.out.println("Execution duration: " + (end - start));
+//
+//        System.out.println("The size of new: " + scraper.getNew().size());
+//        System.out.println("The size of covid: " + scraper.getCovid().size());
+//        System.out.println("The size of politic: " + scraper.getPolitics().size());
+//        System.out.println("The size of business: " + scraper.getBusiness().size());
+//        System.out.println("The size of technology: "  + scraper.getTech().size());
+//        System.out.println("The size of health: " + scraper.getHealth().size());
+//        System.out.println("The size of sport: " + scraper.getSports().size());
+//        System.out.println("The size of entertainment: " + scraper.getEntertainment().size());
+//        System.out.println("The size of world: " + scraper.getWorld().size());
+//        System.out.println("The size of other: " + scraper.getOthers().size());
 
-        System.exit(-1);
+
 
     }
 
